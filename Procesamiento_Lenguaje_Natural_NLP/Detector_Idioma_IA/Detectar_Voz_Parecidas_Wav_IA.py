76b64f3c52b6ce2c9b1be044c1407beafe64935f
unknown <sebasguesa@hotmail.com>
2025-11-27 12:46:12 -0500

    Se adjunta codigo de detectar voz parecida con python

---
+++ Procesamiento_Lenguaje_Natural_NLP/Detector_Idioma_IA/Detectar_Voz_Parecidas_Wav_IA.py
@@ -0,0 +1,60 @@
+
+# En el siguiente codigo se muestra un
+# Detección de la similitud entre 2 audios con voces,
+#  con la libreria de IA Transformers
+# Primero instalamos las librerias necesarias
+# pip install speechbrain torch torchaudio
+
+from resemblyzer import VoiceEncoder, preprocess_wav
+import numpy as np
+import librosa
+
+# Cargar audio y convertir a 16 kHz
+def cargar_audio(path):
+    wav, sr = librosa.load(path, sr=16000)
+    return wav
+
+# Calcular similitud coseno en el audio
+def similitud_coseno(v1, v2):
+    v1 = v1 / np.linalg.norm(v1)
+    v2 = v2 / np.linalg.norm(v2)
+    return float(np.dot(v1, v2))
+
+# En los siguientes codigos se comparan las voces
+def comparar_voces(a1, a2):
+    print("\nSe carga el modelo de voz...")
+    encoder = VoiceEncoder()
+
+    print("\nSe proceso el audio 1...")
+    wav1 = cargar_audio(a1)
+    emb1 = encoder.embed_utterance(wav1)
+
+    print("Se proceso el audio 2...")
+    wav2 = cargar_audio(a2)
+    emb2 = encoder.embed_utterance(wav2)
+
+    print("\nEmpieza a calcular similitud ...")
+    sim = similitud_coseno(emb1, emb2)
+
+    return sim
+
+
+# Main de ejecucion
+if __name__ == "__main__":
+    print("=== DETECTOR DE VOZ PARECIDA (IA – Resemblyzer) ===\n")
+
+    audio1 = input("Ingrese el primer audio (WAV/MP3): ")
+    audio2 = input("Ingrese el segundo audio (WAV/MP3): ")
+
+    sim = comparar_voces(audio1, audio2)
+
+    print(f"La similitud de voces en los 2 audios es de: {sim:.4f}")
+
+    if sim > 0.80:
+        print("Las 2 voces de los audios, son muy parecidas (probablemente la misma persona).")
+    elif sim > 0.60:
+        print("Las 2 voces de los audios, se parecen un poco, pero no hay certeza.")
+    else:
+        print("Las 2 voces de los audios, no se parecen")
+
+    print("--------------------------------------------\n")
